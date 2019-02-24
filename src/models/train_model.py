a617a5d1ad221dda3fc21ea49a88e1a58abd44e8
ff-l <33835310+ff-l@users.noreply.github.com>
2019-02-24 22:18:34 +0000

    Add arguments and split up into basic functions

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,7 +1,10 @@
+import argparse
+import logging.config
 import os
 from datetime import datetime
-
 import keras
+import matplotlib
+matplotlib.use('PS')
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
@@ -244,7 +247,7 @@ def main(edgefile, embeddings_file, batch_size=64, num_samples=[20, 10], layer_s
          dropout=0.1,
          aggregator=AttentionalAggregator, epochs=20):
     """
-    
+
     :param edgefile:
     :param embeddings_file:
     :param batch_size:
@@ -257,40 +260,91 @@ def main(edgefile, embeddings_file, batch_size=64, num_samples=[20, 10], layer_s
     :param epochs:
     :return:
     """
-    input_graph = load_data(edgefile, embeddings_file)
 
+    logger.info("Loading data...")
+    input_graph = load_data(edgefile, embeddings_file)
+    logger.info("Initializing train and test data generators...")
     train, test = init_generators(input_graph, batch_size, num_samples)
+    logger.info("Compiling model...")
     model = compile_model(train, num_samples, layer_sizes, output_act, edge_embedding_method,
                           dropout,
                           aggregator)
+    logger.info("Evaluating untrained model...")
     evaluate_model(model, train, test)
     filename = setup_filename(batch_size, num_samples, layer_sizes, output_act,
                               edge_embedding_method,
                               dropout,
                               aggregator, epochs)
-
+    logger.info("Training model...")
     hist = fit_model(model, train, test, epochs)
+    logger.info("Evaluating trained model...")
     evaluate_model(model, train, test)
-
+    logger.info("Saving model: {}...".filename)
     model.save(os.path.join(MODELS_DIR, filename))
-
+    logger.info("Plotting performance metrics for training...")
     plot_history(hist)
 
 
+def setup_aggregator(agg):
+    if "maxpool" in agg:
+        return MaxPoolingAggregator
+    elif "meanpool" in agg:
+        return MeanPoolingAggregator
+    elif "attent" in agg:
+        return AttentionalAggregator
+    elif "meanagg" in agg:
+        return MeanAggregator
+    else:
+        print("Invalid aggregator, returning MaxPool")
+        return MaxPoolingAggregator
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run graphsage.')
+    parser.add_argument('train_file', help='Name of edge file to train on.')
+    parser.add_argument('emb_file', help='Name of embeddings file to train on.')
+    parser.add_argument('-b', '--batch_size', type=int,
+                        default=64)
+    parser.add_argument('-ns', '--neighbor_samples', type=int, nargs="*",
+                        default=[20, 10])
+    parser.add_argument('-ls', '--layer_size', type=int, nargs="*",
+                        default=[50, 50],
+                        help="The number of hidden features at each GraphSAGE layer")
+    parser.add_argument('-o', '--output_act', type=str,
+                        default="relu")
+    parser.add_argument('-ebm', '--edge_embedding_method', type=str,
+                        default="ip",
+                        help="The method for combining node embeddings into edge embeddings")
+    parser.add_argument('-d', '--dropout', type=float,
+                        default=0.0)
+    parser.add_argument('-a', '--aggregator', type=str,
+                        default="MaxPool")
+    parser.add_argument('-e', '--epochs', type=int,
+                        default=20)
+
+    args = parser.parse_args()
+
+    LOGGING_CONFIG = os.getenv("LOGGING_CONFIG")
+    logging.config.fileConfig(LOGGING_CONFIG)
+    logger = logging.getLogger('train_model')
+
     DATA_DIR = os.getenv("DATA_DIR")
     MODELS_DIR = os.getenv("MODELS_DIR")
     content_api = os.path.join(DATA_DIR, "content_api")
+
     edges = os.path.join(DATA_DIR, "processed_network", "edges_graphsagetest_feb_01_18_doo_min15weight_wtext.csv.gz")
     embeddings = os.path.join(content_api, "training_node_data_fixd.csv.gz")
 
+    logger.info("\n\t{}".format("\n\t".join(["{}: {}".format(arg, getattr(args, arg)) for arg in vars(args)])))
+    
     main(edges,
          embeddings,
-         batch_size=64,
-         num_samples=[20, 10],
-         layer_sizes=[128, 128],
-         output_act="relu",
-         edge_embedding_method="ip",
-         dropout=0.1,
-         aggregator=AttentionalAggregator,
-         epochs=20)
+         batch_size=args.batch_size,
+         num_samples=args.num_samples,
+         layer_sizes=args.neighbor_samples,
+         output_act=args.output_act,
+         # : 'concat', 'mul', 'ip', 'l1', 'l2', or 'avg'"
+         edge_embedding_method=args.edge_embedding_method,
+         dropout=args.dropout,
+         aggregator=setup_aggregator(args.aggregator.lower()),
+         epochs=args.epochs)
