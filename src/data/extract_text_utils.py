83aaeaf85393be5315b62a7dc510750ef9bed51b
ff-l <33835310+ff-l@users.noreply.github.com>
2019-03-05 14:49:42 +0000

    Abstract main functions (flat_extract, nested_extract,
    nested_json_extract). Needs to be tested with dummy data, to
    make sure the aggregation is not scoped out/duplicated
    throughout the functions

--- src/data/extract_text_utils.py
+++ src/data/extract_text_utils.py
@@ -15,6 +15,18 @@ filtered = ['body', 'brand', 'documents', 'final_outcome_detail', 'final_outcome
             'collection_groups']
 
 
+def is_json(raw_text):
+    try:
+        json_normalize(raw_text).columns.tolist()
+    except AttributeError:
+        return False
+    return True
+
+
+def is_html(raw_text):
+    return html.fromstring(str(raw_text)).find('.//*') is not None
+
+
 def extract_text(body):
     """
     Extract text from html body
@@ -50,24 +62,6 @@ def extract_html_links(url):
     return [l for l in links if l.startswith("/")]
 
 
-
-def iterate_json_keys(for_text, x, aggregator):
-    for key, raw_text in sorted(x.items()):
-        if key in filtered:
-            if isinstance(raw_text, str) and len(raw_text) > 1:
-                if for_text:
-                    aggregator = flat_extract(0, raw_text)
-                else:
-                    aggregator.extend(flat_extract(1, raw_text))
-            elif isinstance(raw_text, list) and len(raw_text) > 0:
-                for sub_text in raw_text:
-                    if for_text:
-                        aggregator = nested_extract(for_text, sub_text)
-                    else:
-                        aggregator.extend(nested_extract(1, sub_text))
-    return aggregator
-
-
 # def get_links(x):
 #     links = []
 #     string_json = json.dumps(OrderedDict(x))
@@ -85,57 +79,84 @@ def iterate_json_keys(for_text, x, aggregator):
 #     return list(set(links))
 
 
-def nested_extract(function_type, sub_text="", total_text=""):
-    if function_type == 0:
-        if is_json(sub_text):
-            total_text += nested_iteration(sub_text, "")
-        elif is_html(sub_text):
-            str_from_html = extract_text(sub_text)
-            total_text += " " + str_from_html
-        return total_text
-    else:
-        if is_json(sub_text):
-            return nested_iteration(sub_text, [])
-        elif is_html(sub_text):
-            return extract_html_links(sub_text)
-
-
-def flat_extract(function_type, raw_text="", total_text=""):
-    if function_type == 0:
-        raw_text = raw_text.replace("-", " ")
-        raw_token = raw_text.split(" ")
-        if len(raw_token) > 0:
-            raw_string = extract_text(raw_text)
-            total_text += " " + raw_string
-        return total_text
-    else:
-        return extract_html_links(raw_text)
-
-
-def nested_iteration(x, aggregator):
+def nested_json_extract(aggregator, x):
     """
-Iterate over nested json (avoiding recursion), flattening loops.
+    Iterate over nested json (avoiding recursion), flattening loops.
+    :param aggregator:
     :param x: nested `details` cell contents
     :return: plaintext
     """
 
-    string_json2 = json.dumps(OrderedDict(x))
-    order_json2 = json.loads(string_json2, object_pairs_hook=OrderedDict)
-    if ('body' or 'title') in order_json2.keys():
+    sub_json_str = json.dumps(OrderedDict(x))
+    order_sub_json = json.loads(sub_json_str, object_pairs_hook=OrderedDict)
+    if ('body' or 'title') in order_sub_json.keys():
         for item in look:
             if isinstance(aggregator, str):
-                raw_string2 = extract_text(order_json2[item])
-                if len(raw_string2.split()) > 1:
-                    aggregator += " " + raw_string2
+                sub_raw_str = extract_text(order_sub_json[item])
+                if len(sub_raw_str.split()) > 1:
+                    aggregator += " " + sub_raw_str
             else:
-                aggregator.extend(extract_html_links(order_json2[item]))
-    elif 'child_sections' in order_json2.keys():
-        for child in order_json2['child_sections']:
+                aggregator += extract_html_links(order_sub_json[item])
+    elif 'child_sections' in order_sub_json.keys():
+        for child in order_sub_json['child_sections']:
             for key in child_keys:
                 if isinstance(aggregator, str):
                     aggregator += " " + child[key]
                 else:
                     aggregator.extend(extract_html_links(child))
+    return aggregator
+
+
+def extract_from_details(aggregator, x):
+    """
+
+    :param x:
+    :param aggregator:
+    :return:
+    """
+    for key, raw_text in sorted(x.items()):
+        if key in filtered:
+            if isinstance(raw_text, str) and len(raw_text) > 1:
+                aggregator = flat_extract(aggregator, raw_text)
+            elif isinstance(raw_text, list) and len(raw_text) > 0:
+                for sub_text in raw_text:
+                    aggregator = nested_extract(aggregator, sub_text)
+    return aggregator
+
+
+def flat_extract(aggregator, raw_text=""):
+    """
+
+    :param aggregator:
+    :param raw_text:
+    :return:
+    """
+    if isinstance(aggregator, str):
+        raw_text = raw_text.replace("-", " ")
+        raw_token = raw_text.split(" ")
+        if len(raw_token) > 0:
+            raw_string = extract_text(raw_text)
+            aggregator += " " + raw_string
+    else:
+        aggregator += extract_html_links(raw_text)
+    return aggregator
+
+
+def nested_extract(aggregator, sub_text=""):
+    """
+
+    :param aggregator:
+    :param sub_text:
+    :return:
+    """
+    if is_json(sub_text):
+        aggregator += nested_json_extract(aggregator, sub_text)
+    elif is_html(sub_text):
+        if isinstance(aggregator, str):
+            str_from_html = extract_text(sub_text)
+            aggregator += " " + str_from_html
+        else:
+            aggregator += extract_html_links(sub_text)
     return aggregator
 
 
@@ -146,27 +167,15 @@ reconsider) and extract plaintext from included html.
     :param x: details cell from dataset
     :return: plaintext
     """
-    total_text = ""
+
     if x != "":
-        total_text = iterate_json_keys(1, x)
+        return extract_from_details("", x).strip()
     else:
         links = []
         string_json = json.dumps(OrderedDict(x))
         order_json = json.loads(string_json, object_pairs_hook=OrderedDict)
-        links = iterate_json_keys(0, x)
-    return total_text.strip()
-
-def is_json(raw_text):
-    try:
-        json_normalize(raw_text).columns.tolist()
-    except AttributeError:
-        return False
-    return True
-
-
-def is_html(raw_text):
-    return html.fromstring(str(raw_text)).find('.//*') is not None
-
+        links = extract_from_details(0, x)
+    return total_text
 
 # def nested_extract_text(x):
 #     """
